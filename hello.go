14f4c2622fa9e85d28b354ef1675220d481d5ef6
techkapasi <techkapasi@gmail.com>
2017-06-12 17:04:06 +0500

    test go lang practice program

---
+++ hello.go
@@ -0,0 +1,23 @@
+package main
+
+import ("fmt")
+
+func main() {
+    
+//    defer fmt.Println("\n\nText Written with defer keyword 1")
+//    defer fmt.Println("\n\nText Written with defer keyword 2")
+    
+    fmt.Println("Hello 10Pearls! - GoLang")
+    
+    str := "hello world"
+    fmt.Println("string : ", str)
+    fmt.Println("len : ", len(str))
+    fmt.Println("sub-string (6:11) : ", str[6:11])
+    
+}
+
+
+
+
+
+
