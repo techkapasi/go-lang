80e167d04c3adc5f7369304e803b3048cd82b2cb
hasan.kapasi <hasan.kapasi@10pearls.local>
2017-07-13 16:04:42 +0500

    update in cgo\hello.go file

--- cgo/hello.go
+++ cgo/hello.go
@@ -2,12 +2,14 @@ package main
 
 /*
 #include <stdio.h>
+#include <stdlib.h>
 
-char* greeting = "hello, world";
+char* greeting = "Hello, World ~10Pearls";
 
 */
 import "C"
 import ("fmt")
+
 
 func main() {
 	//stdio.Stdout.WriteString(stdio.Greeting + "\n")
